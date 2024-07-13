f309064bdfa8b2ccbf4c58a136fe6017995e3ed4
우연 <bethelight730@gmail.com>
2024-07-13 09:30:06 +0900

    Feat: AutoLogin

--- Assets/2. Scripts/UIs/ShowRank.cs
+++ Assets/2. Scripts/UIs/ShowRank.cs
@@ -9,6 +9,37 @@ public class ShowRank : MonoBehaviour
     [SerializeField] private TextMeshProUGUI[] nicknameTxt = new TextMeshProUGUI[10];
     [SerializeField] private TextMeshProUGUI[] clearTimeTxt = new TextMeshProUGUI[10];
 
+    public void OnGameOver(float score)
+    {
+        Debug.Log("Your Score:" + score);
+        // currentScore = score;
+        // scoreText.text = $"Your Score: {currentScore}";
+        FetchMyCurrentRank(score);
+    }
+
+    void FetchMyCurrentRank(float score)
+    {
+        // 점수를 제출하고 해당 점수에 대한 랭크를 가져오는 함수
+        string rankUUID = "a9a8c7e0-4065-11ef-8ef5-fbf3130cca85";
+        var bro = Backend.URank.User.GetRankList(rankUUID, 10);
+
+        if (bro.IsSuccess() == false)
+        {
+            Debug.LogError("랭킹 조회중 오류가 발생했습니다. : " + bro);
+            return;
+        }
+        Debug.Log("랭킹 조회에 성공했습니다. : " + bro);
+
+        foreach (LitJson.JsonData jsonData in bro.FlattenRows())
+        {
+            if (score.ToString() == jsonData["score"].ToString())
+            {
+                Debug.Log("랭킹:" + jsonData["rank"].ToString());
+            }
+        }
+    }
+
+
     public void FetchRanking()
     {
         string rankUUID = "a9a8c7e0-4065-11ef-8ef5-fbf3130cca85";
